f4974fad014933a0943a711ce2efcf7f499aff65
VicVerevita <2victor.gabriel01@gmail.com>
2023-08-11 00:13:31 +0300

    day 1

--- exercises/A1/1-basic-syntax/src/bin/03.rs
+++ exercises/A1/1-basic-syntax/src/bin/03.rs
@@ -3,5 +3,28 @@ fn main() {
 
     // TODO
 
-    println!("{} is largest and {} is smallest");
+
+    println!("{} is largest and {} is smallest", largest(&input), smallest(&input));
+}
+
+fn largest(array: &[i32]) -> i32 {
+    let mut largest = 0;
+    for i in 0..array.len() {
+        if array[i] > largest {
+            largest = array[i];
+        }
+    }
+
+    return largest;
+}
+
+fn smallest(array: &[i32]) -> i32 {
+    let mut smallest = array[0];
+    for i in 0..array.len() {
+        if array[i] < smallest {
+            smallest = array[i];
+        }
+    }
+
+    return smallest;
 }
